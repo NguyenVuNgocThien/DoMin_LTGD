689bbb88ea989de7fee6006ecb5d1a008a68a0d3
1951012131Thien@ou.edu.vn <1951012131Thien@ou.edu.vn>
2022-01-15 16:35:56 +0700

    DoMin

---
+++ SoXo/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SoXo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panDoMin = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btStart = new System.Windows.Forms.Button();
+            this.rdCap1 = new System.Windows.Forms.RadioButton();
+            this.rdCap2 = new System.Windows.Forms.RadioButton();
+            this.rdCap3 = new System.Windows.Forms.RadioButton();
+            this.rdCap4 = new System.Windows.Forms.RadioButton();
+            this.rdCap5 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panDoMin
+            // 
+            this.panDoMin.Location = new System.Drawing.Point(12, 42);
+            this.panDoMin.Name = "panDoMin";
+            this.panDoMin.Size = new System.Drawing.Size(545, 514);
+            this.panDoMin.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(881, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewGame,
+            this.saveGameToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // NewGame
+            // 
+            this.NewGame.Name = "NewGame";
+            this.NewGame.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.NewGame.Size = new System.Drawing.Size(246, 30);
+            this.NewGame.Text = "New Game";
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // saveGameToolStripMenuItem
+            // 
+            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
+            this.saveGameToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(246, 30);
+            this.saveGameToolStripMenuItem.Text = "Save Game";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(611, 329);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(130, 45);
+            this.btStart.TabIndex = 3;
+            this.btStart.Text = "Start Game";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // rdCap1
+            // 
+            this.rdCap1.AutoSize = true;
+            this.rdCap1.Location = new System.Drawing.Point(615, 116);
+            this.rdCap1.Name = "rdCap1";
+            this.rdCap1.Size = new System.Drawing.Size(76, 24);
+            this.rdCap1.TabIndex = 4;
+            this.rdCap1.TabStop = true;
+            this.rdCap1.Text = "Cấp 1";
+            this.rdCap1.UseVisualStyleBackColor = true;
+            // 
+            // rdCap2
+            // 
+            this.rdCap2.AutoSize = true;
+            this.rdCap2.Location = new System.Drawing.Point(615, 157);
+            this.rdCap2.Name = "rdCap2";
+            this.rdCap2.Size = new System.Drawing.Size(76, 24);
+            this.rdCap2.TabIndex = 4;
+            this.rdCap2.TabStop = true;
+            this.rdCap2.Text = "Cấp 2";
+            this.rdCap2.UseVisualStyleBackColor = true;
+            // 
+            // rdCap3
+            // 
+            this.rdCap3.AutoSize = true;
+            this.rdCap3.Location = new System.Drawing.Point(615, 199);
+            this.rdCap3.Name = "rdCap3";
+            this.rdCap3.Size = new System.Drawing.Size(76, 24);
+            this.rdCap3.TabIndex = 4;
+            this.rdCap3.TabStop = true;
+            this.rdCap3.Text = "Cấp 3";
+            this.rdCap3.UseVisualStyleBackColor = true;
+            // 
+            // rdCap4
+            // 
+            this.rdCap4.AutoSize = true;
+            this.rdCap4.Location = new System.Drawing.Point(615, 242);
+            this.rdCap4.Name = "rdCap4";
+            this.rdCap4.Size = new System.Drawing.Size(80, 24);
+            this.rdCap4.TabIndex = 4;
+            this.rdCap4.TabStop = true;
+            this.rdCap4.Text = "Cấp 4 ";
+            this.rdCap4.UseVisualStyleBackColor = true;
+            // 
+            // rdCap5
+            // 
+            this.rdCap5.AutoSize = true;
+            this.rdCap5.Location = new System.Drawing.Point(615, 284);
+            this.rdCap5.Name = "rdCap5";
+            this.rdCap5.Size = new System.Drawing.Size(76, 24);
+            this.rdCap5.TabIndex = 4;
+            this.rdCap5.TabStop = true;
+            this.rdCap5.Text = "Cấp 5";
+            this.rdCap5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 626);
+            this.Controls.Add(this.rdCap5);
+            this.Controls.Add(this.rdCap4);
+            this.Controls.Add(this.rdCap3);
+            this.Controls.Add(this.rdCap2);
+            this.Controls.Add(this.rdCap1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.panDoMin);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panDoMin;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewGame;
+        private System.Windows.Forms.ToolStripMenuItem saveGameToolStripMenuItem;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.RadioButton rdCap1;
+        private System.Windows.Forms.RadioButton rdCap2;
+        private System.Windows.Forms.RadioButton rdCap3;
+        private System.Windows.Forms.RadioButton rdCap4;
+        private System.Windows.Forms.RadioButton rdCap5;
+    }
+}
+
---
+++ SoXo/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoXo
+{
+    public partial class Form1 : Form
+    {
+        private int s = 0, w = 0, row = 0, col = 0, slmin = 0;
+        private Button[,] domin;
+        private int[,] a;
+        private int control = 0;
+        private int SoLuongODaMo = 0;
+        private bool[] Level = new bool[5];
+        private void CapDo()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Level[i] = true;
+            }
+            if (rdCap1.Checked == true)
+            {
+                slmin = 2 ;
+                w = 40;
+                row = panDoMin.Width / w;
+                col = panDoMin.Height / w;
+                domin = new Button[row, col];
+                a = new int[row, col];
+                Level[0] = false;
+            }
+            else if (rdCap2.Checked == true)
+            {
+                slmin = 15;
+                w = 35;
+                row = panDoMin.Width / w;
+                col = panDoMin.Height / w;
+                domin = new Button[row, col];
+                a = new int[row, col];
+                Level[1] = false;
+            }
+            else if (rdCap3.Checked == true)
+            {
+                slmin = 20;
+                w = 30;
+                row = panDoMin.Width / w;
+                col = panDoMin.Height / w;
+                domin = new Button[row, col];
+                a = new int[row, col];
+                Level[2] = false;
+            }
+            else if (rdCap4.Checked == true)
+            {
+                slmin = 25;
+                w = 25;
+                row = panDoMin.Width / w;
+                col = panDoMin.Height / w;
+                domin = new Button[row, col];
+                a = new int[row, col];
+                Level[3] = false;
+            }
+            else if (rdCap5.Checked == true)
+            {
+                slmin = 30;
+                w = 20;
+                row = panDoMin.Width / w;
+                col = panDoMin.Height / w;
+                domin = new Button[row, col];
+                a = new int[row, col];
+                Level[4] = false;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    domin[i, j].Width = 0;
+                    domin[i, j].Height = 0;
+                }
+            }
+            control = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (rdCap1.Checked == true || rdCap2.Checked == true || rdCap3.Checked == true || rdCap4.Checked == true || rdCap5.Checked == true)
+            {
+                CapDo();
+                for (int k = 0; k < 5; k++)
+                {
+                    if (!Level[k])
+                    {
+                        int i = 0, j = 0;
+                        for (i = 0; i < row; i++)
+                        {
+                            for (j = 0; j < col; j++)
+                            {
+                                domin[i, j] = new Button();
+                                domin[i, j].Text = "";
+                                domin[i, j].Width = w;
+                                domin[i, j].Height = w;
+                                domin[i, j].Location = new Point(i * w, j * w);
+                                domin[i, j].Click += new EventHandler(Bamnut);
+                                panDoMin.Controls.Add(domin[i, j]);
+                                a[i, j] = 0;
+                            }
+                        }
+                        int dem = 0;
+                        while (dem < slmin)
+                        {
+                            Random rnd = new Random();
+                            i = rnd.Next(0, row);
+                            j = rnd.Next(0, col);
+                            if (a[i, j] == 0)
+                            {
+                                a[i, j] = 1;
+                                dem++;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Bạn chưa chọn cấp độ chơi");
+        }
+
+        private void Bamnut(object sender, EventArgs e)
+        {
+            int x = 0, y = 0;
+            if (control == 0)
+            {
+                
+                x = ((Button)sender).Location.X;
+                y = ((Button)sender).Location.Y;
+                int i = 0, j = 0;
+                i = x / w;
+                j = y / w;
+                if (a[i, j] == 0)
+                {
+                    if (((Button)sender).Text == "")
+                    {
+                        SoLuongODaMo++;
+                        if (SoLuongMin_DuocSetUp(i, j) == 0)
+                        {
+                            domin[i , j].Text = " ";
+                        }
+                        else
+                        {
+                            domin[i , j].Text = SoLuongMin_DuocSetUp(i, j).ToString();
+                        }
+                        ((Button)sender).BackColor = Color.Beige;
+                        if (SoLuongMin_DuocSetUp(i, j) == 0)
+                        {
+                            ClickButton_MoCacButtonLanCan(i, j);
+                        }
+                    }
+                }
+                else
+                {
+                    ((Button)sender).Text = "*";
+                    ((Button)sender).BackColor = Color.Red;
+                    control = -1;
+                }
+            }
+            if (SoLuongODaMo == row * col - slmin)
+            {
+                control = 1;
+            }
+            if (control == 1)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        domin[i, j].Width = 0;
+                        domin[i, j].Height = 0;
+                    }
+                }
+                int l = 0;
+                for (l = 0; l < 5; l++)
+                {
+                    if (Level[l] == false)
+                    {
+                        Level[l] = true;
+                        break;
+                    }
+                }
+                if (l == 0)
+                {
+                    rdCap1.Checked = false;
+                    rdCap2.Checked = true;
+                    SoLuongODaMo = 0;
+                    control = 0;
+                }
+                if (l == 1)
+                {
+                    rdCap2.Checked = false;
+                    rdCap3.Checked = true;
+                    SoLuongODaMo = 0;
+                    control = 0;
+                }
+                if (l == 2)
+                {
+                    rdCap3.Checked = false;
+                    rdCap4.Checked = true;
+                    SoLuongODaMo = 0;
+                    control = 0;
+                }
+                if (l == 3)
+                {
+                    rdCap4.Checked = false;
+                    rdCap5.Checked = true;
+                    SoLuongODaMo = 0;
+                    control = 0;
+                }
+                if (l == 4)
+                {
+                    rdCap5.Checked = false;
+                    SoLuongODaMo = 0;
+                }
+                DialogResult KetQua;
+                KetQua = MessageBox.Show("You win this level", "thong bao", MessageBoxButtons.OK);
+                if (KetQua == DialogResult.OK)
+                {
+                    btStart.PerformClick();
+                }
+            }
+            else if (control == -1)
+            {
+                DialogResult KetQua;
+                KetQua = MessageBox.Show("You lose this level", "thong bao", MessageBoxButtons.OK);
+            }
+        }
+        private void ClickButton_MoCacButtonLanCan(int i, int j)
+        {
+            if (i - 1 > 0) //Kiem tra phia tren
+            {
+                if (a[i - 1, j] == 0)
+                {
+                    if (domin[i - 1, j].Text == "")
+                    {
+                        SoLuongODaMo++;
+                        if (SoLuongMin_DuocSetUp(i, j) == 0)
+                        {
+                            domin[i - 1, j].Text = " ";
+                        }
+                        else
+                        {
+                            domin[i - 1, j].Text = SoLuongMin_DuocSetUp(i, j).ToString();
+                        }
+                        domin[i - 1, j].BackColor = Color.Beige;
+                        if (SoLuongMin_DuocSetUp(i - 1, j) == 0)
+                            ClickButton_MoCacButtonLanCan(i - 1, j);
+                    }
+                }
+            }
+            if (j - 1 > 0) //Kiem tra ben trai
+            {
+                if (a[i, j - 1] == 0)
+                {
+                    if (domin[i, j - 1].Text == "")
+                    {
+                        SoLuongODaMo++;
+                        if (SoLuongMin_DuocSetUp(i, j) == 0)
+                        {
+                            domin[i, j - 1].Text = " ";
+                        }
+                        else
+                        {
+                            domin[i, j - 1].Text = SoLuongMin_DuocSetUp(i, j).ToString();
+                        }
+                        domin[i, j - 1].BackColor = Color.Beige;
+                        if (SoLuongMin_DuocSetUp(i, j - 1) == 0)
+                            ClickButton_MoCacButtonLanCan(i, j - 1);
+                    }
+                }
+            }
+            if (j + 1 > col) //Kiem Tra ben phai
+            {
+                if (a[i, j + 1] == 0)
+                {
+                    if (domin[i, j + 1].Text == "")
+                    {
+                        SoLuongODaMo++;
+                        if (SoLuongMin_DuocSetUp(i, j) == 0)
+                        {
+                            domin[i, j + 1].Text = " ";
+                        }
+                        else
+                        {
+                            domin[i, j + 1].Text = SoLuongMin_DuocSetUp(i, j).ToString();
+                        }
+                        domin[i, j + 1].BackColor = Color.Beige;
+                        if (SoLuongMin_DuocSetUp(i, j + 1) == 0)
+                            ClickButton_MoCacButtonLanCan(i, j + 1);
+                    }
+                }
+            }
+            if (i + 1 > row)
+            {
+                if (a[i + 1, j] == 0)
+                {
+                    if (domin[i + 1, j].Text == "")
+                    {
+                        SoLuongODaMo++;
+                        if (SoLuongMin_DuocSetUp(i, j) == 0)
+                        {
+                            domin[i + 1, j].Text = " ";
+                        }
+                        else
+                        {
+                            domin[i + 1, j].Text = SoLuongMin_DuocSetUp(i, j).ToString();
+                        }
+                        domin[i + 1, j].BackColor = Color.Beige;
+                        if (SoLuongMin_DuocSetUp(i + 1, j) == 0)
+                            ClickButton_MoCacButtonLanCan(i + 1, j);
+                    }
+                }
+            }
+        }
+        private int SoLuongMin_DuocSetUp(int i, int j)
+        {
+            int kq = 0;
+            if (i > 0)
+            {
+                if (j > 0)
+                {
+                    kq += a[i - 1, j - 1];
+                }
+                kq += a[i - 1, j];
+                if (j < col - 1)
+                {
+                    kq += a[i - 1, j + 1];
+                }
+            }
+            if (j > 0)
+            {
+                kq += a[i, j - 1];
+            }
+            if (j < col - 1)
+                kq += a[i, j + 1];
+            if (i < row - 1)
+            {
+                if (j > 0)
+                {
+                    kq += a[i + 1, j - 1];
+                }
+                kq += a[i + 1, j];
+                if (j < col-1)
+                {
+                    kq += a[i + 1, j + 1];
+                }
+            }
+            return kq;
+        }
+    }
+}
